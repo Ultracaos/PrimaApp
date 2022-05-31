1eb68a09de276768fea0f01d3dff3d31e671a0f3
Ultracaos <ultracaosaversa@gmail.com>
2022-05-31 12:13:43 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cocktail',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
